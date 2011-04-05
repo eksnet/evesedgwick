3bafe7549a83d1aea46d6d87aa5ae3706a616f41
pilchard <pilchard.pilchard@gmail.com>
2011-04-05 16:42:09 -0400

    meta rerrangement

--- _plugins/generate_tags.rb
+++ _plugins/generate_tags.rb
@@ -1,4 +1,4 @@
-# Jekyll category page generator.
+# Jekyll tag page generator.
 # http://recursive-design.com/projects/jekyll-plugins/
 #
 # Version: 0.1.3 (201101061053)
@@ -6,10 +6,10 @@
 # Copyright (c) 2010 Dave Perrett, http://recursive-design.com/
 # Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
 #
-# A generator that creates category pages for jekyll sites. 
+# A generator that creates tag pages for jekyll sites. 
 #
 # To use it, simply drop this script into the _plugins directory of your Jekyll site. You should 
-# also create a file called 'category_index.html' in the _layouts directory of your jekyll site 
+# also create a file called 'tag_index.html' in the _layouts directory of your jekyll site 
 # with the following contents (note: you should remove the leading '# ' characters):
 #
 # ================================== COPY BELOW THIS LINE ==================================
@@ -17,12 +17,12 @@
 # layout: default
 # ---
 # 
-# <h1 class="category">{{ page.title }}</h1>
+# <h1 class="tag">{{ page.title }}</h1>
 # <ul class="posts">
-# {% for post in site.categories[page.category] %}
+# {% for post in site.tags[page.tag] %}
 #     <div>{{ post.date | date_to_html_string }}</div>
 #     <h2><a href="{{ post.url }}">{{ post.title }}</a></h2>
-#     <div class="categories">Filed under {{ post.categories | category_links }}</div>
+#     <div class="tags">Filed under {{ post.tags | tag_links }}</div>
 # {% endfor %}
 # </ul>
 # ================================== COPY ABOVE THIS LINE ==================================
@@ -30,44 +30,44 @@
 # You can alter the _layout_ setting if you wish to use an alternate layout, and obviously you
 # can change the HTML above as you see fit. 
 #
-# When you compile your jekyll site, this plugin will loop through the list of categories in your 
+# When you compile your jekyll site, this plugin will loop through the list of tags in your 
 # site, and use the layout above to generate a page for each one with a list of links to the 
 # individual posts.
 #
 # Included filters :
-# - category_links:      Outputs the list of categories as comma-separated <a> links.
+# - tag_links:      Outputs the list of tags as comma-separated <a> links.
 # - date_to_html_string: Outputs the post.date as formatted html, with hooks for CSS styling.
 #
 # Available _config.yml settings :
-# - category_dir:          The subfolder to build category pages in (default is 'categories').
-# - category_title_prefix: The string used before the category name in the page title (default is 
-#                          'Category: ').
+# - tag_dir:          The subfolder to build tag pages in (default is 'tags').
+# - tag_title_prefix: The string used before the tag name in the page title (default is 
+#                          'Tag: ').
 module Jekyll
   
   
-  # The CategoryIndex class creates a single category page for the specified category.
-  class CategoryIndex < Page
+  # The TagIndex class creates a single tag page for the specified tag.
+  class TagIndex < Page
     
-    # Initializes a new CategoryIndex.
+    # Initializes a new TagIndex.
     #
     #  +base+         is the String path to the <source>.
-    #  +category_dir+ is the String path between <source> and the category folder.
-    #  +category+     is the category currently being processed.
-    def initialize(site, base, category_dir, category)
+    #  +tag_dir+ is the String path between <source> and the tag folder.
+    #  +tag+     is the tag currently being processed.
+    def initialize(site, base, tag_dir, tag)
       @site = site
       @base = base
-      @dir  = category_dir
+      @dir  = tag_dir
       @name = 'index.html'
       self.process(@name)
       # Read the YAML data from the layout page.
-      self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
-      self.data['category']    = category
+      self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
+      self.data['tag']    = tag
       # Set the title for this page.
-      title_prefix             = site.config['category_title_prefix'] || 'Category: '
-      self.data['title']       = "#{title_prefix}#{category}"
+      title_prefix             = site.config['tag_title_prefix'] || 'Tag: '
+      self.data['title']       = "#{title_prefix}#{tag}"
       # Set the meta-description for this page.
-      meta_description_prefix  = site.config['category_meta_description_prefix'] || 'Category: '
-      self.data['description'] = "#{meta_description_prefix}#{category}"
+      meta_description_prefix  = site.config['tag_meta_description_prefix'] || 'Tag: '
+      self.data['description'] = "#{meta_description_prefix}#{tag}"
     end
     
   end
@@ -76,82 +76,66 @@ module Jekyll
   # The Site class is a built-in Jekyll class with access to global site config information.
   class Site
     
-    # Creates an instance of CategoryIndex for each category page, renders it, and 
+    # Creates an instance of TagIndex for each tag page, renders it, and 
     # writes the output to a file.
     #
-    #  +category_dir+ is the String path to the category folder.
-    #  +category+     is the category currently being processed.
-    def write_category_index(category_dir, category)
-      index = CategoryIndex.new(self, self.source, category_dir, category)
+    #  +tag_dir+ is the String path to the tag folder.
+    #  +tag+     is the tag currently being processed.
+    def write_tag_index(tag_dir, tag)
+      index = TagIndex.new(self, self.source, tag_dir, tag)
       index.render(self.layouts, site_payload)
       index.write(self.dest)
       # Record the fact that this page has been added, otherwise Site::cleanup will remove it.
       self.pages << index
     end
     
-    # Loops through the list of category pages and processes each one.
-    def write_category_indexes
-      if self.layouts.key? 'category_index'
-        dir = self.config['category_dir'] || '/'
-        self.categories.keys.each do |category|
-          self.write_category_index(File.join(dir, category), category)
+    # Loops through the list of tag pages and processes each one.
+    def write_tag_indexes
+      if self.layouts.key? 'tag_index'
+        dir = self.config['tag_dir'] || '/tag'
+        self.tags.keys.each do |tag|
+          self.write_tag_index(File.join(dir, tag), tag)
         end
         
       # Throw an exception if the layout couldn't be found.
       else
-        throw "No 'category_index' layout found."
+        throw "No 'tag_index' layout found."
       end
     end
     
   end
   
   
-  # Jekyll hook - the generate method is called by jekyll, and generates all of the category pages.
-  class GenerateCategories < Generator
+  # Jekyll hook - the generate method is called by jekyll, and generates all of the tag pages.
+  class GenerateTags < Generator
     safe true
     priority :low
 
     def generate(site)
       puts "entering generate with #{site}"
-      site.write_category_indexes
+      site.write_tag_indexes
     end
 
   end
   
   
-  # Adds some extra filters used during the category creation process.
+  # Adds some extra filters used during the tag creation process.
   module Filters
     
-    # Outputs a list of categories as comma-separated <a> links. This is used
-    # to output the category list for each post on a category page.
+    # Outputs a list of tags as comma-separated <a> links. This is used
+    # to output the tag list for each post on a tag page.
     #
-    #  +categories+ is the list of categories to format.
+    #  +tags+ is the list of tags to format.
     #
     # Returns string
-    def category_links(categories)
-      categories = categories.sort!.map do |item|
-        '<a href="/category/'+item+'/">'+item+'</a>'
-      end
-      
-      connector = "and"
-      case categories.length
-      when 0
-        ""
-      when 1
-        categories[0].to_s
-      when 2
-        "#{categories[0]} #{connector} #{categories[1]}"
-      else
-        "#{categories[0...-1].join(', ')}, #{connector} #{categories[-1]}"
-      end
-    end
-
     def tag_links(tags)
       tags = tags.sort!.map do |item|
-        '<a href="/category/'+item+'/">'+item+'</a>'
+        if item
+          '<a href="/tag/'+item+'/">'+item.upcase+'</a>'
+        end
       end
       
-      connector = "and"
+      connector = " "
       case tags.length
       when 0
         ""
